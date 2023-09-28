e2b1f71af132f396abd1aefe3d91f7d42ad201f3
Sherin Olivia <SOChronicle@gmail.com>
2023-09-28 16:07:45 +0700

    feat: created new path for profile and altered variable in .env.example

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import './App.css'
 import  {BrowserRouter, Route, Routes } from 'react-router-dom'
-import { CreateNew, Login, Register, UpdateCategory, CategoryList } from './pages'
+import { CreateNew, Login, Register, UpdateCategory, CategoryList, Profile } from './pages'
+import AppProvider from './provider/AppProvider'
 
 function App() {
   
   
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} /> 
-        <Route path='/register' element={<Register />} />
-        <Route path='/' element={<CategoryList />} />
-        <Route path='/add' element={<CreateNew />} />
-        <Route path='/edit/:id' element={<UpdateCategory />} />
-      </Routes>
-    </BrowserRouter>
+
+      <BrowserRouter>
+        <AppProvider>
+          <Routes>
+            <Route path='/login' element={<Login />} /> 
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<CategoryList />} />
+            <Route path='/add' element={<CreateNew />} />
+            <Route path='/edit/:id' element={<UpdateCategory />} />
+            <Route path='/profile' element={<Profile />} />
+          </Routes>
+        </AppProvider>
+      </BrowserRouter>
+
   )
 }
 
